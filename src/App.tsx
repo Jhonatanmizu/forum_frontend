961ea18a234cab98ba2644f28d35d0d2bc653f12
jhonatanmizu <natanjesuss20@gmail.com>
2024-07-20 10:56:41 -0300

    chore: add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <main>
       <h1 className="text-xl text-red-300">HADOUKEN</h1>
+      <Button>Click me</Button>
     </main>
   );
 };
