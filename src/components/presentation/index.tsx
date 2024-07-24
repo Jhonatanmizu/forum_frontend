3557e6b545c6830f7fb682bfdeb9f34253197753
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-07-23 22:49:07 -0300

    :sparkles: added full presentation component

---
+++ src/components/presentation/index.tsx
@@ -0,0 +1,15 @@
+import PresentationTitle from "./title";
+import PresentationSchedules from "./schedules";
+import PresentationSubtitle from "./subtitle";
+
+const Presentation = () => {
+  return (
+    <>
+      <PresentationTitle />
+      <PresentationSchedules />
+      <PresentationSubtitle />
+    </>
+  );
+};
+
+export default Presentation;
