6a961046a5689f9812d19b2e1c0ab2a9f59563f2
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-07-23 22:47:58 -0300

    :sparkles: added presentation subtitles

---
+++ src/components/presentation/subtitle.tsx
@@ -0,0 +1,15 @@
+const PresentationSubtitle = () => {
+  return (
+    <div className="flex flex-col text-center text-white">
+      <p className="text-lg md:text-xl">CONTAGEM REGRESSIVA PARA VOCÊ SABER</p>
+      <p className="text-lg md:text-xl">
+        COMO{" "}
+        <span className="text-secondary">
+          A INTELIGÊNCIA ARTIFICIAL VAI MUDAR A SUA VIDA E O SEU FUTURO.
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default PresentationSubtitle;
