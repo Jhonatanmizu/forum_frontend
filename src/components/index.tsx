182e18535bbbc9039b9e8d0d572ad187e0748b0e
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-07-23 22:53:46 -0300

    :sparkles: added import facilitator

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import Header from "./header";
+import CountDown from "./countdown";
+import { Button } from "./ui/button";
+import Presentation from "./presentation";
+import SubscribeButton from "./subscribeButton";
+
+export { Header, CountDown, Button, Presentation, SubscribeButton };
