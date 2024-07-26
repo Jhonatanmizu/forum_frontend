2a5404c6277a361280f7b8c139b2a54aa47cb8aa
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-07-26 20:51:40 -0300

    :bricks: updated import centralizer

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,5 +3,13 @@ import CountDown from "./countdown";
 import { Button } from "./ui/button";
 import Presentation from "./presentation";
 import SubscribeButton from "./subscribeButton";
+import SpeakersCarousel from "./speakersCarousel";
 
-export { Header, CountDown, Button, Presentation, SubscribeButton };
+export {
+  Header,
+  CountDown,
+  Button,
+  Presentation,
+  SubscribeButton,
+  SpeakersCarousel,
+};
