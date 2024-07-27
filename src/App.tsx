caa9d0e902047509f720eebf8761078d04406d04
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-07-26 22:55:44 -0300

    :sparkles: feat: miniCourses Session implemented

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {
   Presentation,
   SubscribeButton,
   SpeakersCarousel,
+  FullMiniCoursesCards,
 } from "./components/";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
       <CountDown />
       <SubscribeButton />
       <SpeakersCarousel />
+      <FullMiniCoursesCards />
     </main>
   );
 };
