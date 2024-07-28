ecb6b360bccde497d56d4268be64401d279c3cd6
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-07-28 10:44:30 -0300

    :sparkles: full timeline component has created

---
+++ src/components/timeline/index.tsx
@@ -0,0 +1,44 @@
+//Components
+import TimeLineItem from "./timeLine";
+
+//Data
+import { thursdayEvents, fridayEvents } from "./data";
+
+//Imagens
+import WhiteRobot from "../../assets/images/white-robot.png";
+
+const CompleteTimeLine = () => {
+  return (
+    <div className="flex flex-col self-center items-center gap-5 w-[95%] md:w-full h-fit mt-16">
+      <p className="text-text text-2lg md:text-xl">cronograma do evento</p>
+      <div
+        className="flex flex-col md:flex-row rounded-xl md:rounded-none
+                      bg-gradient-to-b md:bg-gradient-to-r from-primary/20 to-background-1"
+      >
+        <div className="ml-5 mt-14 md:mb-16">
+          <p className="text-secondary font-semibold text-2lg md:text-xl">
+            DIA 29
+          </p>
+          {thursdayEvents.map((item, index) => (
+            <TimeLineItem key={index} {...item} />
+          ))}
+        </div>
+        <div className="ml-5 mt-14 mb-14 md:mb-16">
+          <p className="text-secondary font-semibold text-2lg md:text-xl">
+            DIA 30
+          </p>
+          {fridayEvents.map((item, index) => (
+            <TimeLineItem key={index} {...item} />
+          ))}
+          <img
+            src={WhiteRobot}
+            alt="white robot image"
+            className="flex h-[352px] md:hidden"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CompleteTimeLine;
