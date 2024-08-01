27d1207a3c66a6e8f82404c4e33af91a8d909e4f
jhonatanmizu <natanjesuss20@gmail.com>
2024-07-31 23:47:01 -0300

    :recycle: chore: add comment in app tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,8 @@ const App = () => {
     <main className="flex flex-col bg-background-1 max-w-screen min-h-screen h-fit">
       <Header />
       {/* TODO CREATE A BLUR COMPONENT HERE */}
+      {/* We should create a section or div to blur parts, and this section should use position relative,
+      and you should control de position of the blur using absolute position */}
       <div
         className="absolute z-10 bg-primary/25
                       w-full h-[425px]
