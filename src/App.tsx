5dcd0f937b5a2a11768233e667123b0d582117e1
jhonatanmizu <natanjesuss20@gmail.com>
2024-07-31 23:42:29 -0300

    :bug: fix: broken margin at right

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,28 @@ const App = () => {
   return (
     <main className="flex flex-col bg-background-1 max-w-screen min-h-screen h-fit">
       <Header />
+      {/* TODO CREATE A BLUR COMPONENT HERE */}
+      <div
+        className="absolute z-10 bg-primary/25
+                      w-full h-[425px]
+                       md:h-[700px]
+                      top-20 right-1/2 transform -translate-x-1/2
+                      rounded-full blur-[400px]"
+      />
+      <div
+        className="absolute z-10 bg-secondary/25
+                    w-full h-[425px]
+                  md:h-[700px]
+                      rounded-full blur-[400px]
+                      right-1 transform -translate-x-1/4"
+      />
+
       <Presentation />
       <CountDown />
+      <div
+        className="absolute self-center bg-primary/45 w-full h-[425px] md:h-[700px]
+                   rounded-full blur-[400px] overflow-hidden"
+      />
       <SubscribeButton />
       <SpeakersCarousel />
       <FullMiniCoursesCards />
--- src/components/presentation/schedules.tsx
+++ src/components/presentation/schedules.tsx
@@ -3,23 +3,8 @@ import BlueRobot from "../../assets/images/blue-robot.png";
 
 const PresentationSchedules = () => {
   return (
-    <div className="flex flex-row max-w-full text-center justify-center items-center text-white mt-5 overflow-hidden">
-      <div
-        className="absolute z-10 bg-primary/25
-                      w-[425px] h-[425px]
-                      md:w-[700px] md:h-[700px]
-                      top-20 right-1/2 transform -translate-x-1/2
-                      rounded-full blur-[400px]"
-      />
-      <div
-        className="absolute z-10 bg-secondary/25
-                      w-[425px] h-[425px]
-                      md:w-[700px] md:h-[700px]
-                      rounded-full blur-[400px]
-                      right-1 transform -translate-x-1/4"
-      />
-
-      <div className="text-center h-fit absolute top-36 font-extrabold text-white/5 blur-[5px] overflow-hidden">
+    <div className="flex flex-row max-w-full text-center relative justify-center items-center text-white mt-5 overflow-hidden">
+      <div className="text-center h-fit absolute md:-top-28 -top-10  font-extrabold text-white/5 blur-[5px] overflow-hidden">
         <p className="text-[88px] md:text-[280px]">FUTURO</p>
       </div>
 
